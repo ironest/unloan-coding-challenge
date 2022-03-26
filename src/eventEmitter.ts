8d29e1349cb981706128107201501646896bb599
Riccardo Carzaniga <carzaniga.riccardo@gmail.com>
2022-03-26 23:51:27 +1000

    Added internal property to hold client registrations

--- src/eventEmitter.ts
+++ src/eventEmitter.ts
@@ -1,8 +1,18 @@
+interface IListener {
+  [key: string]: Function[];
+}
+
 export class EventEmitter {
-  constructor() {}
+  private listeners: IListener;
+
+  constructor() {
+    this.listeners = {};
+  }
+
   register(eventType: string, handler: Function) {
     throw new Error("Method not implemented.");
   }
+
   emit(eventType: string, payload: Object) {
     throw new Error("Method not implemented.");
   }
